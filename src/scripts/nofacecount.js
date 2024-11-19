c1bd91a24c274a8fbf3359432d17cb5b150c1dd6
oageo <kig.osumiakari+gh@gmail.com>
2024-11-20 00:29:43 +0900

    onclickevent関数を作成

--- src/scripts/nofacecount.js
+++ src/scripts/nofacecount.js
@@ -1,9 +1,14 @@
+const onclickevent = () => {
+    ding.currentTime = 0;
+    ding.play();
+    nofacecount++;
+    document.getElementById("displaycount").textContent = nofacecount;
+}
+
 let nofacecount = 0;
 const nofacebuttonpush = document.getElementById("nofacebutton");
 const ding = new Audio("/assets/otologic-ding04-short.opus");
 ding.currentTime = 0;
 nofacebuttonpush.addEventListener("click", () => {
-    ding.currentTime = 0;
-    nofacecount++;
-    document.getElementById("displaycount").textContent = nofacecount;
+    onclickevent();
 })
